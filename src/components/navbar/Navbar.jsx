74166ffc44c3fced689194dc1448e18dd1a50685
unknown <anoldmwawasi@gmail.com>
2024-07-08 21:01:47 +0300

    Make links dynamic

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,46 +1,64 @@
 import React from "react";
-import logo from "../../assets/logo.png"
+import Logo from "../../assets/Logo2.png";
+
+const menu = [
+  {
+    id: 1,
+    name: "Home",
+    link: "/#",
+  },
+  {
+    id: 2,
+    name: "About Us",
+    link: "/about",
+  },
+  {
+    id: 3,
+    name: "Internet Plans",
+    link: "/internet-plans",
+  },
+  {
+    id: 4,
+    name: "Customer Support",
+    link: "/customer-support",
+  },
+  {
+    id: 5,
+    name: "Terms of Service",
+    link: "/terms-of-service",
+  },
+  {
+    id: 6,
+    name: "Contact Us",
+    link: "/contact",
+  },
+];
 
 const Navbar = () => {
   return (
-    <div className="bg-blue-200 shadow-md">
+    <div className="bg-blue-200 shadow-md duration-200 relative z-50">
       <div className="container mx-auto px-4 lg:px-6 xl:px-8">
         <div className="flex justify-between items-center py-4">
           {/* Left side of navbar */}
           <div className="flex justify-between items-center gap-2 text-2xl font-bold text-gray-800">
-            <a href="#">
-              <img className="w-5  animate-pulse" src={logo} alt="Logo" />
+            <a href="/">
+              <img className="w-5 animate-bounce" src={Logo} alt="Logo" />
             </a>
-            <span>Freedom Wireless Solutions</span>
+            <span>Kwetu computers</span>
           </div>
 
           {/* Right side of navbar */}
           <ul className="hidden md:flex space-x-4 text-gray-800">
-            <li>
-              <a href="#" className="hover:text-blue-500 font-semibold">
-                About Us
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:text-blue-500 font-semibold">
-                Internet Plans
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:text-blue-500 font-semibold">
-                Customer Support
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:text-blue-500 font-semibold">
-                Terms of Service
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:text-blue-500 font-semibold">
-                Contact Us
-              </a>
-            </li>
+            {menu.map((item) => (
+              <li key={item.id}>
+                <a
+                  href={item.link}
+                  className="hover:text-blue-500 font-semibold"
+                >
+                  {item.name}
+                </a>
+              </li>
+            ))}
           </ul>
 
           {/* Mobile menu button (hidden on larger screens) */}
@@ -64,46 +82,16 @@ const Navbar = () => {
         {/* Mobile menu (hidden by default) */}
         <div className="md:hidden hidden">
           <ul className="flex flex-col space-y-2">
-            <li>
-              <a
-                href="#"
-                className="block py-2 px-4 text-gray-800 hover:bg-blue-100 font-semibold"
-              >
-                About Us
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 px-4 text-gray-800 hover:bg-blue-100 font-semibold"
-              >
-                Internet Plans
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 px-4 text-gray-800 hover:bg-blue-100 font-semibold"
-              >
-                Customer Support
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 px-4 text-gray-800 hover:bg-blue-100 font-semibold"
-              >
-                Terms of Service
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 px-4 text-gray-800 hover:bg-blue-100 font-semibold"
-              >
-                Contact Us
-              </a>
-            </li>
+            {menu.map((item) => (
+              <li key={item.id}>
+                <a
+                  href={item.link}
+                  className="block py-2 px-4 text-gray-800 hover:bg-blue-100 font-semibold"
+                >
+                  {item.name}
+                </a>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
@@ -112,3 +100,4 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
